1a57fce31fcd031756307ba03c375cbbe8de8cd8
John Tweeddale <j.tweeddale@outlook.com>
2020-11-09 16:32:48 +0000

    finish main logic

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -8,5 +8,32 @@ class GamesController < ApplicationController
   end
 
   def score
+    
+    search = params[:word].upcase
+    given_letters = params[:letter_grid]
+    url = "https://wagon-dictionary.herokuapp.com/#{search}"
+    read_url = open(url).read
+    json_result = JSON.parse(read_url)    
+    
+    def not_exceed_given_letters(search, given_letters)
+      ok = true      
+      search.each_char do |char|
+        search.count(char) > given_letters.count(char) ? ok = false : ok = true
+      end
+      ok
+    end
+    
+    def exists_in_dict?(json_result)
+      json_result["found"] ? true : false
+    end
+
+    if !not_exceed_given_letters(search, given_letters)
+      @response = "Sorry but #{search} cannot be built out of #{given_letters}"
+    elsif !exists_in_dict?(json_result)
+      @response = "Sorry but #{search} does not seem to be a valid English word"
+    else
+      @response = "Congratulations! #{search} is a valid English word!" 
+    end 
+    
   end
 end
